ac006e378f0cdbde565d2f2401dc944f48d61ba1
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-12-03 23:02:08 +0900

    分野

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,9 +1,16 @@
 class FoodItem
   include ActiveHash::Base
   self.data = [
-    { id: 1, name: '料理1' },
-    { id: 2, name: '料理2' },
-    { id: 3, name: '料理3' },
-    # 必要なだけデータを追加
+    { id: 1, name: 'サラダ' },
+    { id: 2, name: 'お肉' },
+    { id: 3, name: '魚' },
+    { id: 4, name: 'ご飯' },
+    { id: 5, name: 'パン' },
+    { id: 6, name: '麺' },
+    { id: 7, name: '水' },
+    { id: 8, name: 'お茶' },
+    { id: 9, name: 'コーヒー' },
+    { id: 10, name: 'ジュース' },
+    { id: 11, name: 'その他' }
   ]
 end
