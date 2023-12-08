b752897227b7baae14d074b20d206242725f71d3
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-12-08 22:38:19 +0900

    コントローラーとdeviseの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_07_134651) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_08_133622) do
   create_table "activity_details", charset: "utf8", force: :cascade do |t|
     t.bigint "dategraphs_id"
     t.integer "calories_consumed"
@@ -29,6 +29,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_07_134651) do
     t.datetime "e_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "activity_details", "dategraphs", column: "dategraphs_id"
