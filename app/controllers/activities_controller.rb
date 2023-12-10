ac7b9cd9e1b1f6cdc3edab44e91b5dcb588519e2
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-12-10 23:09:54 +0900

    Update activities_controller.rb

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,14 @@
 class ActivitiesController < ApplicationController
+  def index
+    @activities = Activity.all
+  end
+
+  def new
+    @activities = Activity.new
+  end
+
+  def create
+    Activity.create(activity_params)
+    redirect_to '/'
+  end
 end
